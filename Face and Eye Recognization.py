d29d2b28f3a9f893c9a58baaf09e4d58406a410f
Gaurav Maurya <92087933+gaurav9927@users.noreply.github.com>
2022-01-12 13:56:01 +0530

    Add files via upload

---
+++ Face and Eye Recognization.py
@@ -0,0 +1,10 @@
+# Face Recognition
+
+# Importing the libraries
+import cv2
+
+# Loading the cascades
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+# Defining a function that will do the detections
